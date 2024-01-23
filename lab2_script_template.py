8a72ee6426cfeaefa159a9562539add337e21c05
JordanHall13 <jordan.hall@flemingcollege.ca>
2024-01-23 10:31:19 -0500

    def print name
    added our name and id to the function

--- lab2_script_template.py
+++ lab2_script_template.py
@@ -21,8 +21,12 @@ def main():
     about_me["movies"]
 # TODO: Step 4 - Function that prints student name and ID	
 def print_student_name_and_id(about_me):
+    full_name = about_me["full_name"]
+    first_name = full_name.split()[0]
+    student_id = about_me["student_id"]
+    print(f"My name is {full_name}, but you can call me Mr. {first_name}.\nMy student ID is {student_id}")
     return
-    
+
 # TODO: Step 5 - Function that adds pizza toppings to data structure
 def add_pizza_toppings(about_me, toppings):
     return
