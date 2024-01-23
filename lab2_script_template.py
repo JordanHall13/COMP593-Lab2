ea468708d4afd61bad3de6b276de9184930c7e7a
JordanHall13 <jordan.hall@flemingcollege.ca>
2024-01-23 10:25:29 -0500

    step 2 finished
    completed step 2

---
+++ lab2_script_template.py
@@ -0,0 +1,43 @@
+def main():
+
+    # TODO: Step 2 - Create a complex data structure
+    about_me = {
+        "full_name": "Jordan Hall",
+        "student_id": 10227505,
+        "pizza_toppings": ["PEPPERONI", "GREEN PEPPERS", "BACON"],
+        "movies": [
+            {
+                "title": "scott pilgrim",
+                "genre": "action comedy"
+            },
+            {
+                "title": "hot rod",
+                "genre": "comedy"
+            }
+        ]
+    }
+
+    # TODO: Step 3 - Add another movie to the data structure
+    about_me["movies"]
+# TODO: Step 4 - Function that prints student name and ID	
+def print_student_name_and_id(about_me):
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
